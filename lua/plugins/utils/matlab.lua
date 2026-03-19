e7d2dc89dbfba5ff0149d8fc9782ecadcc19491c
andreacaldato-hub <andreacaldato6@gmail.com>
2026-03-19 18:50:22 +0100

    remove toggleterm from matlab config

--- lua/plugins/utils/matlab.lua
+++ lua/plugins/utils/matlab.lua
@@ -1,28 +1,5 @@
 return {
 	{
-		"akinsho/toggleterm.nvim",
-		version = "*",
-		opts = {
-			open_mapping = [[<c-\>]],
-			direction = "float",
-			float_opts = {
-				border = "curved",
-				width = 60,
-				height = 20,
-				winblend = 3,
-				row = 1,
-				col = vim.o.columns - 65,
-			},
-		},
-		keys = {
-			{ "<c-\\>", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
-			{ "<leader>tt", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
-			{ "<leader>tc", "<cmd>ToggleTerm<cr>", desc = "Close terminal" },
-			-- from inside the terminal, use <C-\><C-n> to go to normal mode
-			-- then these work too
-		},
-	},
-	{
 		"idossha/matlab.nvim",
 		ft = "matlab",
 		config = function()
