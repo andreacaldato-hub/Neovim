afb50e5a5a743a3e428e4f45d6c3c2cfa4a326cb
andreacaldato-hub <andreacaldato6@gmail.com>
2026-04-20 19:46:35 +0200

    i dont know

--- init.lua
+++ init.lua
@@ -1,28 +1,28 @@
 require("config.lazy")
 require("plugins.latex.snippets")
 local function set_blink_hl()
-	vim.api.nvim_set_hl(0, "BlinkCmpMenu", { bg = "NONE", fg = "#c0c0c0" })
-	vim.api.nvim_set_hl(0, "BlinkCmpMenuSelection", { bg = "#1f2329", fg = "NONE", bold = true })
-	vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", { fg = "#3D3D3D" })
-	vim.api.nvim_set_hl(0, "BlinkCmpLabel", { fg = "NONE", bg = "NONE" })
-	vim.api.nvim_set_hl(0, "BlinkCmpLabelDescription", { fg = "#c8c093", bg = "NONE" })
-	vim.api.nvim_set_hl(0, "BlinkCmpDoc", { fg = "#c0c0c0", bg = "NONE" })
-	vim.api.nvim_set_hl(0, "BlinkCmpDocBorder", { fg = "#1f2329" })
-	vim.api.nvim_set_hl(0, "CursorLine", { bg = "NONE" })
-	vim.api.nvim_set_hl(0, "BlinkCmpLabelMatch", {
+  vim.api.nvim_set_hl(0, "BlinkCmpMenu", { bg = "NONE", fg = "#c0c0c0" })
+  vim.api.nvim_set_hl(0, "BlinkCmpMenuSelection", { bg = "#1f2329", fg = "NONE", bold = true })
+  vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", { fg = "#3D3D3D" })
+  vim.api.nvim_set_hl(0, "BlinkCmpLabel", { fg = "NONE", bg = "NONE" })
+  vim.api.nvim_set_hl(0, "BlinkCmpLabelDescription", { fg = "#c8c093", bg = "NONE" })
+  vim.api.nvim_set_hl(0, "BlinkCmpDoc", { fg = "#c0c0c0", bg = "NONE" })
+  vim.api.nvim_set_hl(0, "BlinkCmpDocBorder", { fg = "#3D3D3D" })
+  vim.api.nvim_set_hl(0, "CursorLine", { bg = "NONE" })
+  vim.api.nvim_set_hl(0, "BlinkCmpLabelMatch", {
 
-		fg = "NONE", -- Usa lo stesso colore che hai impostato per "BlinkCmpLabel"
-		bg = "NONE",
-		bold = false,
-		nocombine = true, -- Importante: impedisce ad altri hl di combinarsi sopra
-	})
+    fg = "NONE", -- Usa lo stesso colore che hai impostato per "BlinkCmpLabel"
+    bg = "NONE",
+    bold = false,
+    nocombine = true, -- Importante: impedisce ad altri hl di combinarsi sopra
+  })
 
-	vim.api.nvim_set_hl(0, "String", { fg = "#89b482", italic = true }) -- Gruvbox Aqua
-	vim.api.nvim_set_hl(0, "BlinkCmpLabelMatchDescription", {
-		fg = "NONE",
-		bg = "NONE",
-		bold = false,
-	})
+  vim.api.nvim_set_hl(0, "String", { fg = "#89b482", italic = true }) -- Gruvbox Aqua
+  vim.api.nvim_set_hl(0, "BlinkCmpLabelMatchDescription", {
+    fg = "NONE",
+    bg = "NONE",
+    bold = false,
+  })
 end
 vim.api.nvim_create_autocmd({ "VimEnter", "ColorScheme" }, { callback = set_blink_hl })
 -- This removes the background color from all floating windows
--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,10 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Blink.cmp documentation window border color (VSCode-like)
+
+-- Pointer asterisk highlighting
+vim.api.nvim_set_hl(0, "@operator", { fg = "#F14C4C" })
+
+-- LSP Hover/Documentation window border
