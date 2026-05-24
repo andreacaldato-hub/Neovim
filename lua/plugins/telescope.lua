56a593e34c1922897b1fc9ee53770199c1c9c1a4
andreacaldato-hub <andreacaldato6@gmail.com>
2026-05-24 20:10:57 +0200

    change telescope colors

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -47,7 +47,7 @@ return {
           bg = "NONE",
           bold = true,
         })
-        vim.api.nvim_set_hl(0, "TelescopeMatching", { fg = "#89b482", bold = false })
+        vim.api.nvim_set_hl(0, "TelescopeMatching", { fg = "#FE8E40", bold = false })
       end
 
       vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
